842863724e58b83f822161fe54151b56f04d9e44
nbarrett <nick.barrett36@me.com>
2026-02-16 22:22:29 +0200

    build: bump version to 1.7.5

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -1,4 +1,4 @@
-export const APP_VERSION = "1.7.4";
+export const APP_VERSION = "1.7.5";
 
 export interface ChangelogEntry {
   version: string;
@@ -8,6 +8,14 @@ export interface ChangelogEntry {
 }
 
 export const CHANGELOG: ChangelogEntry[] = [
+  {
+    version: "1.7.5",
+    date: "16 Feb 2026",
+    title: "Bug fixes",
+    changes: [
+      "Increase vertical padding on drive detail page",
+    ],
+  },
   {
     version: "1.7.4",
     date: "16 Feb 2026",
